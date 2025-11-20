1fb93e2ebee8f47d37baf850042294cbfa026972
Ruben Justiniano <ruben.justiniano96@gmail.com>
2025-11-20 16:10:41 -0400

    chore: setup postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgre',
-    password: 'root',
-    database: 'restaurant_db',
-    entities: [],
-    synchronize: true,
-  }),],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: String(process.env.DB_PASSWORD),
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
